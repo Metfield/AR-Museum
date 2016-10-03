50da4dd234d7eda883a3e16b47c23c9978376617
John Petersson <johnpetersson@gmail.com>
2016-10-03 18:14:51 +0200

    Initial

---
+++ app/src/test/java/ciu196/chalmers/se/armuseum/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ciu196.chalmers.se.armuseum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
