dc507885fffb5bd26ddfa52c22bcfce98f1b5f10
John Petersson <johnpetersson@gmail.com>
2016-10-10 14:22:52 +0200

    Sending to database working

--- app/src/main/java/ciu196/chalmers/se/armuseum/PaintActivity.java
+++ app/src/main/java/ciu196/chalmers/se/armuseum/PaintActivity.java
@@ -1,14 +1,38 @@
 package ciu196.chalmers.se.armuseum;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PaintActivity extends AppCompatActivity {
 
+    private static final String TAG = "PaintActivity";
+
+
+    // Firebase instance variables
+    private DatabaseReference mFirebaseDatabaseReference;
+//    private FirebaseRecyclerAdapter<FriendlyMessage, MessageViewHolder>
+//            mFirebaseAdapter;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    public static final String MESSAGES_CHILD = "messages";
+
+
     private DrawingView drawingView;
 
+    private Button sendButton;
     private Button drawButton;
     private Button eraseButton;
 
@@ -34,7 +58,78 @@ public class PaintActivity extends AppCompatActivity {
                 drawingView.setErasing(true);
             }
         });
+
+        setupFirebase();
+        login();
+
+        sendButton = (Button) findViewById(R.id.button_send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FriendlyMessage mess = new FriendlyMessage("textcontent", "namename", "url");
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(mess);
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void setupFirebase() {
+        // Authentication
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        // Database
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void login() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+                            Toast.makeText(PaintActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
 
 
 }
