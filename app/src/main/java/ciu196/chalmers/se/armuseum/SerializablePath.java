d04aa16a22c0948d5d520187df3208e7ad6868ec
John Petersson <johnpetersson@gmail.com>
2016-10-11 13:40:10 +0200

    Smoother orbital shift through velocity diff

--- app/src/main/java/ciu196/chalmers/se/armuseum/SerializablePath.java
+++ app/src/main/java/ciu196/chalmers/se/armuseum/SerializablePath.java
@@ -1,6 +1,7 @@
 package ciu196.chalmers.se.armuseum;
 
 import android.graphics.Path;
+import android.widget.AbsListView;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
@@ -19,110 +20,133 @@ import java.util.List;
  */
 public class SerializablePath extends Path implements Serializable {
 
-    private List<Action> actions = new LinkedList();
+    private String name = "TestName";
+    private ChildClass child = new ChildClass();
 
-    // Empty Database Constructor
     public SerializablePath() {
 
     }
 
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException{
-        in.defaultReadObject();
+    public String getName() {
+        return name;
+    }
 
-        for (Action action : actions) {
-            action.perform(this);
+    private class ChildClass implements Serializable {
+        private String childName = "ChildName";
+
+        public ChildClass() {
+
+        }
+
+        public String getChildName() {
+            return childName;
         }
     }
 
-    @Override
-    public void lineTo(float x, float y) {
-        actions.add(new Line(x, y));
-        super.lineTo(x, y);
-    }
-
-    @Override
-    public void moveTo(float x, float y) {
-        actions.add(new Move(x, y));
-        super.moveTo(x, y);
-    }
-
-    @Override
-    public void reset() {
-        actions.clear();
-        super.reset();
-    }
-
-    private interface Action extends Serializable {
-        void perform(Path path);
-    }
-
-    private static final class Line implements Action {
-
-        private float x, y;
-
-        public Line() {}
-
-        public Line(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public void perform(Path path) {
-            path.lineTo(x, y);
-        }
-
-        public void setX(float x) {
-            this.x = x;
-        }
-
-        public void setY(float y) {
-            this.y = y;
-        }
-
-        public float getX() {
-            return x;
-        }
-
-        public float getY() {
-            return y;
-        }
-    }
-
-    private static final class Move implements Action {
-
-        private float x, y;
-
-        public Move() {}
-
-        public Move(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public void perform(Path path) {
-            path.moveTo(x, y);
-        }
-
-        public void setX(float x) {
-            this.x = x;
-        }
-
-        public void setY(float y) {
-            this.y = y;
-        }
-
-        public float getX() {
-            return x;
-        }
-
-        public float getY() {
-            return y;
-        }
-    }
-
-    public List<Action> getActions() {
-        return actions;
-    }
+//    private List<Action> actions = new LinkedList();
+//
+//    // Empty Database Constructor
+//    public SerializablePath() {
+//
+//    }
+//
+//    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException{
+//        in.defaultReadObject();
+//
+//        for (Action action : actions) {
+//            action.perform(this);
+//        }
+//    }
+//
+//    @Override
+//    public void lineTo(float x, float y) {
+//        actions.add(new Line(x, y));
+//        super.lineTo(x, y);
+//    }
+//
+//    @Override
+//    public void moveTo(float x, float y) {
+//        actions.add(new Move(x, y));
+//        super.moveTo(x, y);
+//    }
+//
+//    @Override
+//    public void reset() {
+//        actions.clear();
+//        super.reset();
+//    }
+//
+//    private interface Action extends Serializable {
+//        void perform(Path path);
+//    }
+//
+//    private static final class Line implements Action {
+//
+//        private float x, y;
+//
+//        public Line() {}
+//
+//        public Line(float x, float y) {
+//            this.x = x;
+//            this.y = y;
+//        }
+//
+//        @Override
+//        public void perform(Path path) {
+//            path.lineTo(x, y);
+//        }
+//
+//        public void setX(float x) {
+//            this.x = x;
+//        }
+//
+//        public void setY(float y) {
+//            this.y = y;
+//        }
+//
+//        public float getX() {
+//            return x;
+//        }
+//
+//        public float getY() {
+//            return y;
+//        }
+//    }
+//
+//    private static final class Move implements Action {
+//
+//        private float x, y;
+//
+//        public Move() {}
+//
+//        public Move(float x, float y) {
+//            this.x = x;
+//            this.y = y;
+//        }
+//
+//        @Override
+//        public void perform(Path path) {
+//            path.moveTo(x, y);
+//        }
+//
+//        public void setX(float x) {
+//            this.x = x;
+//        }
+//
+//        public void setY(float y) {
+//            this.y = y;
+//        }
+//
+//        public float getX() {
+//            return x;
+//        }
+//
+//        public float getY() {
+//            return y;
+//        }
+//    }
+//
+//    public List<Action> getActions() {
+//        return actions;
+//    }
 }
